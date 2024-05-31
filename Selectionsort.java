9ae431b25e55d26f11c0658bba094149ef11a385
Thenuka Dilmith Liyanage <115771525+dilmith456@users.noreply.github.com>
2024-05-31 21:32:45 +0530

    Add files via upload

---
+++ Selectionsort.java
@@ -0,0 +1,42 @@
+package datastructures;
+
+public class Selectionsort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int[] arr = {64, 25, 12, 22, 11};
+	        
+	        System.out.println("Array before sorting:");
+	        printArray(arr);
+	        
+	        selectionSort(arr);
+	        
+	        System.out.println("Array after sorting:");
+	        printArray(arr);
+	}
+
+	
+	public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the minimum element with the current element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+	public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
